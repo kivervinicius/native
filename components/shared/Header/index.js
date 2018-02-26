b2c49ed43de542ecc1c691a0000ceb82e71a71bc
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-26 19:26:02 -0300

    Add header logo onPress handler

--- components/shared/Header/index.js
+++ components/shared/Header/index.js
@@ -1,18 +1,22 @@
-import {Component, Fragment} from 'react'
-import {StatusBar, SafeAreaView} from 'react-native'
+import {Fragment} from 'react'
+import {StatusBar, SafeAreaView, TouchableHighlight} from 'react-native'
 
 import Logo from '@/components/shared/Logo'
 import styles from './styles'
 
-export default class Header extends Component {
-  render() {
-    return (
-      <Fragment>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={styles.container}>
+export default function Header({navigation, scene}) {
+  let gotoHome
+  if (scene.route.routeName !== 'home')
+    gotoHome = () => navigation.navigate('home')
+
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <TouchableHighlight onPress={gotoHome} style={styles.logo}>
           <Logo height={40} />
-        </SafeAreaView>
-      </Fragment>
-    )
-  }
+        </TouchableHighlight>
+      </SafeAreaView>
+    </Fragment>
+  )
 }
