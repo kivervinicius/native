baba9cf9312f07950b317cbd3f4c73ac98181d97
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-26 19:26:10 -0300

    Add header styles

--- components/shared/Header/styles.js
+++ components/shared/Header/styles.js
@@ -2,6 +2,10 @@ import {Platform, StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: Platform.OS === 'ios' ? 20 : 0
   }
 })
