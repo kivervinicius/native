0838264ea97cc57d45f7d693014f7943f01b7df0
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-26 20:07:31 -0300

    Add redux provider

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
+import {Provider} from 'react-redux'
 import {StackNavigator} from 'react-navigation'
+
+import createStore from '@/redux'
 import * as views from '@/components/views'
 
-export default StackNavigator(views, {
+const Stack = StackNavigator(views, {
   initialRouteName: 'home'
 })
+
+const store = createStore()
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Stack />
+    </Provider>
+  )
+}
