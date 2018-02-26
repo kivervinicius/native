e62c17ddde66eb4beb7b9b139cfe9706ccf63f8b
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-26 17:30:03 -0300

    Add status bar to header

--- components/shared/Header/index.js
+++ components/shared/Header/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {StatusBar, SafeAreaView} from 'react-native'
 
 import Logo from '@/components/shared/Logo'
 import Container from './styles'
@@ -6,9 +7,12 @@ import Container from './styles'
 export default class Header extends Component {
   render() {
     return (
-      <Container>
-        <Logo height={40} />
-      </Container>
+      <SafeAreaView>
+        <StatusBar barStyle="dark-content" />
+        <Container>
+          <Logo height={40} />
+        </Container>
+      </SafeAreaView>
     )
   }
 }
--- components/shared/Header/styles.js
+++ components/shared/Header/styles.js
@@ -1,3 +1,6 @@
+import {Platform} from 'react-native'
 import styled from 'styled-components'
 
-export default styled.View``
+export default styled.View`
+  margin-top: ${Platform.OS === 'ios' ? 20 : 0};
+`
