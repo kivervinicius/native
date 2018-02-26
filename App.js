fcfffd9f3baf8c70b0cc061ba04e2b5a65853215
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-26 16:02:48 -0300

    Add react-navigation

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
-import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StackNavigator} from 'react-navigation'
+import {Landing, Listings} from '@/src/components/views'
 
-import Logo from '@/src/components/shared/Logo'
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Logo />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    )
+export default StackNavigator(
+  {
+    Landing: {
+      screen: Landing
+    },
+    Listings: {
+      screen: Listings
+    }
+  },
+  {
+    initialRouteName: 'Landing'
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+)
