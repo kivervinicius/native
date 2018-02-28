10f75bb7b7f1da7ba777a3980f024f571f887df3
Gabriela Seabra <gabrielaseabra@Gabrielas-MacBook-Pro.local>
2018-02-28 16:57:20 -0300

    Fix hmr

--- containers/App.js
+++ containers/App.js
@@ -1,9 +1,10 @@
+import {Component} from 'react'
 import {StackNavigator} from 'react-navigation'
 
 import Header from '@/containers/shared/Header'
 import * as views from '@/containers/views'
 
-export default StackNavigator(views, {
+const Stack = StackNavigator(views, {
   initialRouteName: 'home',
   headerMode: 'screen',
   navigationOptions: {
@@ -11,3 +12,11 @@ export default StackNavigator(views, {
     header: (props) => <Header {...props} />
   }
 })
+
+// Wrap StackNavigator in a Component class for hot module replacement
+// https://github.com/facebook/react-native/issues/8465
+export default class App extends Component {
+  render() {
+    return <Stack />
+  }
+}
