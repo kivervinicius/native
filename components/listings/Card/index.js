4e2195373062a8afaa6600a027021e5cdf8cea24
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-07 20:33:56 -0300

    Add listings card image

--- components/listings/Card/index.js
+++ components/listings/Card/index.js
@@ -1,12 +1,14 @@
-import {View, Text} from 'react-native'
+import {View, Text, Dimensions} from 'react-native'
 
 import Image from '../Image'
+
+const imageWidth = Math.min(400, Dimensions.get('window').width - 50)
 
 export default function ListingCard({images, ...props}) {
   const image = images[0] || {}
   return (
     <View>
-      <Image thumbnail {...image} />
+      <Image thumbnail width={imageWidth} {...image} />
       <Text>{console.log(props)}</Text>
     </View>
   )
