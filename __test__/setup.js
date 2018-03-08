b000f9b6e2602e09bc2241ac6770326d2698cb32
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-08 14:47:42 -0300

    Sync mocks

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,1 +1,8 @@
-jest.mock('react-native-fabric')
+import fs from 'fs'
+import path from 'path'
+
+const MOCKS_DIR = path.resolve(__dirname, '../__mocks__')
+
+fs.readdirSync(MOCKS_DIR).map((filename) => {
+  jest.mock(filename.replace(/\.[^/.]+$/, ''))
+})
