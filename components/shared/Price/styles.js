56c4c25bd81d84afaec9136d663ce2511640f792
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-07 21:16:43 -0300

    Add Price component

---
+++ components/shared/Price/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center'
+  },
+  currency: {
+    marginTop: 2
+  }
+})
