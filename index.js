b33a4a5850865391641f21a867dcb86018eef563
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-09 13:14:37 -0300

    Remove react-native warnings

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import {AppRegistry} from 'react-native'
+import {AppRegistry, YellowBox} from 'react-native'
 
 import App from '@/containers/App'
 import Provider from '@/containers/Provider'
+
+// Temporary fix for
+// https://github.com/facebook/react-native/issues/18175
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated'
+])
 
 AppRegistry.registerComponent('EmCasa', () => App)
 
