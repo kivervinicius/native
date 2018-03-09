2b60172a277397ba7256905da3d6938ec9460007
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-09 13:23:20 -0300

    Disable RCTImageLoader warning

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import App from '@/containers/App'
 import Provider from '@/containers/Provider'
 
 // Temporary fix for
-// https://github.com/facebook/react-native/issues/18175
+// https://github.com/facebook/react-native/issues/
+// https://github.com/facebook/react-native/issues/17504
 YellowBox.ignoreWarnings([
   'Warning: componentWillMount is deprecated',
-  'Warning: componentWillReceiveProps is deprecated'
+  'Warning: componentWillUpdate is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Module RCTImageLoader'
 ])
 
 AppRegistry.registerComponent('EmCasa', () => App)
