7eea2ba1585f8c933435ed2304960e622b170336
Gabriela Seabra <gabriela.seabra@emcasa.com>
2018-03-10 00:09:19 -0300

    Add ListingsSearch state

--- components/listings/Search/index.js
+++ components/listings/Search/index.js
@@ -1,21 +1,44 @@
+import _ from 'lodash'
+import update from 'immutability-helper'
 import {Component} from 'react'
 import {SafeAreaView, View} from 'react-native'
 
-import Range from './Range'
 import styles from './styles'
 
 export default class ListingsSearch extends Component {
+  state = {
+    visible: [],
+    value: {}
+  }
+
+  static defaultProps = {
+    value: {}
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      value: _.defaults({}, props.value, state.value, {
+        area: {min: undefined, max: undefined},
+        price: {min: undefined, max: undefined},
+        rooms: {min: undefined, max: undefined},
+        neighborhoods: []
+      })
+    }
+  }
+
+  onChange = (field) => (value) =>
+    this.setState((state) => ({
+      value: update(state.value, {
+        [field]: {$set: value}
+      })
+    }))
+
+  onSubmit = () => this.props.onSubmit(this.state.value)
+
   render() {
     return (
       <SafeAreaView style={styles.wrapper}>
-        <View style={styles.container}>
-          <Range
-            options={[
-              {label: 'foo', value: 'foo'},
-              {label: 'bar', value: 'bar'}
-            ]}
-          />
-        </View>
+        <View style={styles.container}>...</View>
       </SafeAreaView>
     )
   }
