4084cfe730527face775820fc9d15c90480971f9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-13 13:15:32 -0300

    Add number formatter

---
+++ assets/format.js
@@ -0,0 +1,2 @@
+export const number = (num) =>
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
--- components/shared/Price/index.js
+++ components/shared/Price/index.js
@@ -1,5 +1,6 @@
 import {View, Text} from 'react-native'
 
+import * as format from '@/assets/format'
 import styles from './styles'
 
 export default function Price({children, size, ...props}) {
@@ -8,9 +9,7 @@ export default function Price({children, size, ...props}) {
   return (
     <View style={styles.container} {...props}>
       <Text style={[styles.currency, currency]}>R$</Text>
-      <Text style={[styles.amount, amount]}>
-        {Number(children).toLocaleString()}
-      </Text>
+      <Text style={[styles.amount, amount]}>{format.number(children)}</Text>
     </View>
   )
 }
