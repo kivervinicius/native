cb372e073a1efff43cd336a35254340e514ac3c8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-13 18:20:04 -0300

    Use static dimensions on listings gallery

--- components/listings/View/index.js
+++ components/listings/View/index.js
@@ -1,11 +1,44 @@
+import {Component} from 'react'
 import {View, Text} from 'react-native'
 
+import Modal from '@/components/shared/Modal'
+import Gallery from '../Gallery'
 import styles from './styles'
 
-export default function ListingView({description}) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.description}>{description}</Text>
-    </View>
-  )
+export default class ListingView extends Component {
+  state = {
+    view: 'gallery'
+  }
+
+  onOpen = (view) => () => this.setState({view})
+
+  onOpenGallery = this.onOpen('gallery')
+
+  onClose = () => this.setState({view: undefined})
+
+  renderGallery() {}
+  renderModal() {
+    const {images} = this.props
+    const {view} = this.state
+
+    switch (view) {
+      case 'gallery':
+        return <Gallery>{images}</Gallery>
+      default:
+        return undefined
+    }
+  }
+
+  render() {
+    const {description} = this.props
+    const {view} = this.state
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>{description}</Text>
+        <Modal overlay visible={Boolean(view)} onDismiss={this.onClose}>
+          {this.renderModal()}
+        </Modal>
+      </View>
+    )
+  }
 }
