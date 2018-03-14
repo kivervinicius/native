f83a94bdaf2670882c1ab3090cc87b8c4ca6697e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-14 15:16:56 -0300

    Add Shell scroll property

--- components/shared/Shell/index.js
+++ components/shared/Shell/index.js
@@ -1,13 +1,14 @@
-import {SafeAreaView, View} from 'react-native'
+import {SafeAreaView, ScrollView, View} from 'react-native'
 
 import Navigation from './Navigation'
 import styles from './styles'
 
-export default function Shell({children, header, ...props}) {
+export default function Shell({children, header, scroll, ...props}) {
+  const Main = scroll ? ScrollView : View
   return (
     <SafeAreaView style={styles.container}>
       {header && <View style={styles.header}>{header}</View>}
-      <View style={styles.main}>{children}</View>
+      <Main style={styles.main}>{children}</Main>
       <View style={styles.nav}>
         <Navigation {...props} />
       </View>
