5c8c27bad7cf91b0dfaa162332124f5c1f0e658d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-14 17:09:55 -0300

    Fix Gallery dimensions getter

--- components/listings/Gallery/index.js
+++ components/listings/Gallery/index.js
@@ -11,8 +11,6 @@ export default class ListingGallery extends Component {
     position: 0
   }
 
-  dimensions = Dimensions.get('window')
-
   onSwipe = ({nativeEvent}) => {
     const position = nativeEvent.contentOffset.x / this.dimensions.width
     const index = Math.round(position)
@@ -21,6 +19,10 @@ export default class ListingGallery extends Component {
 
   get items() {
     return this.props.children
+  }
+
+  get dimensions() {
+    return Dimensions.get('window')
   }
 
   renderPagination() {
