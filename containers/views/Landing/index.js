b7ddab0cc493a1fe37e56d0b3bd98e8b5300062e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-15 18:59:11 -0300

    Remove "hello world"

--- containers/views/Landing/index.js
+++ containers/views/Landing/index.js
@@ -6,7 +6,6 @@ import Listings from '@/containers/listings/Feed'
 export default function LandingScreen() {
   return (
     <Shell>
-      <Text style={{textAlign: 'center'}}>Hello World</Text>
       <Listings type="featured" />
     </Shell>
   )
