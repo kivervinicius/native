3fb233844e565f0383e392e0b24a629aa7fecd62
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-15 00:10:35 -0300

    Update stylesheet usage

--- components/shared/Modal/index.js
+++ components/shared/Modal/index.js
@@ -1,25 +1,27 @@
 import {Modal, View, Text, SafeAreaView, TouchableOpacity} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-import styles from './styles'
+import $styles from './styles'
 
 export default function ControlledModal({
   children,
   title,
   closeIcon,
   onDismiss,
+  overlay,
   ...props
 }) {
+  const styles = $styles({overlay})
   return (
     <Modal {...props} onRequestClose={onDismiss} onDismiss={onDismiss}>
-      <SafeAreaView style={styles.container()}>
-        <View style={styles.header(props)}>
-          <TouchableOpacity style={styles.closeButton()} onPress={onDismiss}>
-            <Icon name={closeIcon} style={styles.closeIcon()} />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity style={styles.closeButton} onPress={onDismiss}>
+            <Icon name={closeIcon} style={styles.closeIcon} />
           </TouchableOpacity>
-          {title && <Text style={styles.title()}>{title}</Text>}
+          {title && <Text style={styles.title}>{title}</Text>}
         </View>
-        <View style={styles.body()}>{children}</View>
+        <View style={styles.body}>{children}</View>
       </SafeAreaView>
     </Modal>
   )
