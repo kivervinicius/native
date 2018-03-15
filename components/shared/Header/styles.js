3ac23b54a4c30600c516b4e152b5754fa468ffb4
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-15 19:06:10 -0300

    Header border

--- components/shared/Header/styles.js
+++ components/shared/Header/styles.js
@@ -1,8 +1,12 @@
 import {StyleSheet} from 'react-native'
 
+import * as colors from '@/assets/colors'
+
 export default StyleSheet.create({
   container: {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    borderColor: colors.gray.light,
+    borderBottomWidth: StyleSheet.hairlineWidth
   },
   wrapper: {
     display: 'flex',
