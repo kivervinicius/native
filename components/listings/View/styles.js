cb11ed536a991ebe840cf127718efe0979967805
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-15 23:55:08 -0300

    Listings View header border

--- components/listings/View/styles.js
+++ components/listings/View/styles.js
@@ -1,8 +1,14 @@
 import {StyleSheet} from 'react-native'
 
+import * as colors from '@/assets/colors'
+
 export default StyleSheet.create({
   container: {
     flex: 1,
     display: 'flex'
+  },
+  header: {
+    borderColor: colors.gray.light,
+    borderBottomWidth: StyleSheet.hairlineWidth
   }
 })
