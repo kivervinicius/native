d93692ac3b7c7a997b72fef9c7da3de348c469ed
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-24 18:23:34 -0300

    Add header

--- fastlane/provision.sh
+++ fastlane/provision.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 filename=$IOS_MOBILE_PROVISION
 uuid=`grep UUID -A1 -a $filename | grep -io "[-A-F0-9]\{36\}"`
 name=`grep "<key>Name" -A1 -a $filename | pcregrep -o1 '<string>(.*)</string>'`
