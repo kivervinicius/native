4efca5f56d717bc452ca685cc3ab16094ee06b6f
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-24 18:50:41 -0300

    fix provision script

--- fastlane/provision.sh
+++ fastlane/provision.sh
@@ -4,6 +4,7 @@ filename=$IOS_MOBILE_PROVISION
 uuid=`grep UUID -A1 -a $filename | grep -io "[-A-F0-9]\{36\}"`
 name=`grep "<key>Name" -A1 -a $filename | pcregrep -o1 '<string>(.*)</string>'`
 
+mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
 cp $filename "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
 
 echo $uuid
