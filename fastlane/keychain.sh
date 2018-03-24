a83fce7dab95deab1c0a04c6182de48a34904ae2
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-23 23:09:22 -0300

    Fix keychain.sh output

--- fastlane/keychain.sh
+++ fastlane/keychain.sh
@@ -6,14 +6,18 @@ keychain_file="$HOME/Library/Keychains/$keychain_name-db"
 
 security create-keychain -p $keychain_pass $keychain_name
 
-
 security unlock-keychain -p $keychain_pass $keychain_file
 
 [[ ! -z $IOS_CERTIFICATE_CER ]] && \
-  security import $IOS_CERTIFICATE_CER -k $keychain_file -T /usr/bin/codesign
+  security import $IOS_CERTIFICATE_CER \
+    -k $keychain_file \
+    -T /usr/bin/codesign > /dev/null
 
 [[ ! -z $IOS_CERTIFICATE_P12 ]] && \
-  security import $IOS_CERTIFICATE_P12 -k $keychain_file -P "$IOS_CERTIFICATE_PASSWORD" -T /usr/bin/codesign
+  security import $IOS_CERTIFICATE_P12 \
+    -k $keychain_file \
+    -P "$IOS_CERTIFICATE_PASSWORD" \
+    -T /usr/bin/codesign > /dev/null
 
 security set-keychain-settings -t 3600 -l $keychain_file
 
