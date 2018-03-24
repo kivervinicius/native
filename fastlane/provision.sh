7866f25c0b7b0473bf5309074fe30e41df7ffd41
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-24 02:14:34 -0300

    fix provisioning profile definition

--- fastlane/provision.sh
+++ fastlane/provision.sh
@@ -1,6 +1,8 @@
 filename=$IOS_MOBILE_PROVISION
 uuid=`grep UUID -A1 -a $filename | grep -io "[-A-F0-9]\{36\}"`
+name=`grep "<key>Name" -A1 -a $filename | pcregrep -o1 '<string>(.*)</string>'`
 
 cp $filename "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
 
 echo $uuid
+echo $name
