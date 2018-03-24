66036a901c961daf7fb2f458b50253fd71d9a43b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-23 22:59:37 -0300

    Add keychain script

---
+++ fastlane/keychain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+keychain_name=${2:-'ci.keychain'}
+keychain_pass=$1
+keychain_file="$HOME/Library/Keychains/$keychain_name-db"
+
+security create-keychain -p $keychain_pass $keychain_name
+
+
+security unlock-keychain -p $keychain_pass $keychain_file
+
+[[ ! -z $IOS_CERTIFICATE_CER ]] && \
+  security import $IOS_CERTIFICATE_CER -k $keychain_file -T /usr/bin/codesign
+
+[[ ! -z $IOS_CERTIFICATE_P12 ]] && \
+  security import $IOS_CERTIFICATE_P12 -k $keychain_file -P "$IOS_CERTIFICATE_PASSWORD" -T /usr/bin/codesign
+
+security set-keychain-settings -t 3600 -l $keychain_file
+
+echo $keychain_file
