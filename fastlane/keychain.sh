73e458c04d1a2b39edad41e11f3cc503daf5dd1e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-25 15:49:58 -0300

    fix keychain args

--- fastlane/keychain.sh
+++ fastlane/keychain.sh
@@ -5,10 +5,9 @@ keychain_pass=$1
 keychain_file="$HOME/Library/Keychains/$keychain_name-db"
 
 security create-keychain -p $keychain_pass $keychain_name
-
-security unlock-keychain -p $keychain_pass $keychain_file
-
 security default-keychain -s $keychain_name
+security unlock-keychain -p $keychain_pass $keychain_name
+security set-keychain-settings -t 3600 -l $keychain_file
 
 [[ ! -z $IOS_CERTIFICATE_CER ]] && \
   security import $IOS_CERTIFICATE_CER \
@@ -20,7 +19,5 @@ security default-keychain -s $keychain_name
     -k $keychain_file \
     -P "$IOS_CERTIFICATE_PASSWORD" \
     -T /usr/bin/codesign > /dev/null
-
-security set-keychain-settings -t 3600 -l $keychain_file
 
 echo $keychain_file
