057fb024843765ec129227ba0567890136e7c84e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-28 16:16:26 -0300

    Fix style combiner

--- assets/StyleSheet/combine.js
+++ assets/StyleSheet/combine.js
@@ -4,8 +4,9 @@ export default (styles) =>
   _.flow(
     (...args) => args,
     _.map((value) => {
-      if (typeof value === 'string' && value in styles) return styles[value]
-      return value
+      if (typeof value !== 'string') return value
+      else if (value in styles) return styles[value]
+      return undefined
     }),
     _.concat([])
   )
