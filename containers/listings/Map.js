bf0828528a221cfa1b62ada2cf66e58e44e84268
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-03-28 18:01:07 -0300

    map markers key prop

--- containers/listings/Map.js
+++ containers/listings/Map.js
@@ -2,7 +2,11 @@ import {withFeed} from './Feed'
 import Map, {Marker} from '@/components/listings/Map'
 
 function MapApp({data}) {
-  return <Map>{data && data.map((listing) => <Marker {...listing} />)}</Map>
+  return (
+    <Map>
+      {data && data.map((listing) => <Marker key={listing.id} {...listing} />)}
+    </Map>
+  )
 }
 
 export default withFeed(MapApp)
