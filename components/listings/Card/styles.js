b6d43466f0299a4a1bb9333143699cbbe5f4be7b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-02 14:52:31 -0300

    Primary card style

--- components/listings/Card/styles.js
+++ components/listings/Card/styles.js
@@ -21,23 +21,42 @@ export default NestedStyleSheet({
     borderRadius: 5,
     ':small': {
       borderRadius: 3
+    },
+    ':primary': {
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      borderTopWidth: 4,
+      borderColor: colors.blue.medium,
+      backgroundColor: colors.blue.medium,
+      marginTop: -4
     }
   },
   image: {
+    width: '100%',
     borderRadius: 5,
     ':small': {
       borderRadius: 3
+    },
+    ':primary': {
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0
     }
   },
   body: {
-    padding: 15
+    padding: 15,
+    ':small': {
+      padding: 20
+    }
   },
   header: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 10
+    marginBottom: 10,
+    ':small': {
+      marginBottom: 5
+    }
   },
   street: {
     color: colors.gray.darker,
@@ -45,7 +64,9 @@ export default NestedStyleSheet({
     fontSize: 20,
     marginRight: 15,
     ':small': {
-      fontSize: 14
+      fontSize: 14,
+      fontWeight: '500',
+      marginTop: 10
     }
   },
   neighborhood: {
