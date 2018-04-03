46597c5e8dfc7d577db4032e9021e7b2f20d3411
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-02 22:03:54 -0300

    MultiSelect button styles

--- components/listings/Search/Field/MultiSelect/styles.js
+++ components/listings/Search/Field/MultiSelect/styles.js
@@ -3,6 +3,13 @@ import {StyleSheet} from 'react-native'
 import * as colors from '@/assets/colors'
 
 export default StyleSheet.create({
+  options: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginLeft: 10,
+    marginRight: 10
+  },
   button: {
     display: 'flex',
     flexDirection: 'row',
@@ -18,4 +25,31 @@ export default StyleSheet.create({
   }
 })
 
-export const button = StyleSheet.create({})
+export const button = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: colors.gray.$f0f0,
+    margin: 5,
+    paddingLeft: 10,
+    paddingRight: 5
+  },
+  text: {
+    color: colors.gray.mediumDark
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    fontSize: 13,
+    lineHeight: 18,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: colors.gray.mediumDark,
+    color: colors.gray.mediumDark,
+    marginLeft: 10
+  }
+})
