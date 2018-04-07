fc8a9d53d29990c4cc943140c33710c282641062
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-07 01:58:57 -0300

    ios setup script

---
+++ .buildkite/setup-ios.sh
@@ -0,0 +1,14 @@
+set -e
+
+mkdir -p .secrets
+
+echo "Installing certificate"
+
+aws s3 sync $SECRETS_BUCKET .secrets --quiet
+
+export IOS_XCCONFIG_FILE=$PWD/.secrets/ad-hoc/app.xcconfig
+export IOS_CERTIFICATE_FILE=$PWD/.secrets/ad-hoc/app.p12
+export IOS_PROVISIONING_FILE=~$PWD/.secrets/ad-hoc/app.mobileprovision
+export IOS_PROVISIONING_UUID=`grep UUID -A1 -a $IOS_PROVISIONING_FILE | grep -io "[-A-F0-9]\{36\}"`
+export IOS_PROVISIONING_NAME=`grep "<key>Name" -A1 -a $IOS_PROVISIONING_FILE | pcregrep -o1 '<string>(.*)</string>'`
+
