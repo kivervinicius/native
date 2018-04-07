d5d27ee52ec9c1190b55aa0f52bf12e4c26ff98b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-07 16:42:20 -0300

    buildkite hooks

--- .buildkite/setup-android.sh
+++ .buildkite/setup-android.sh
@@ -1,21 +1,10 @@
 set -eu
-set -x
 
 PATH=$PATH:$ANDROID_HOME/tools/bin
-
-mkdir .secrets
-
-# Install keystore
 
 echo "Installing android sdk"
 
 sdkmanager "platforms;android-26"
 sdkmanager "build-tools;26.0.1"
 sdkmanager "extras;android;m2repository" "extras;google;m2repository"
-
-echo "Installing keystore"
-
-aws s3 sync $AWS_SECRETS_BUCKET .secrets
-
-export ANDROID_KEYSTORE_FILE=$PWD/.secrets/keystore.jks
 
--- .buildkite/setup-ios.sh
+++ .buildkite/setup-ios.sh
@@ -1,15 +1,1 @@
 set -eu
-set -x
-
-mkdir -p .secrets
-
-echo "Installing certificate"
-
-aws s3 sync $AWS_SECRETS_BUCKET .secrets
-
-export IOS_XCCONFIG_FILE=$PWD/.secrets/ad-hoc/app.xcconfig
-export IOS_CERTIFICATE_FILE=$PWD/.secrets/ad-hoc/app.p12
-export IOS_PROVISIONING_FILE=~$PWD/.secrets/ad-hoc/app.mobileprovision
-export IOS_PROVISIONING_UUID=`grep UUID -A1 -a $IOS_PROVISIONING_FILE | grep -io "[-A-F0-9]\{36\}"`
-export IOS_PROVISIONING_NAME=`grep "<key>Name" -A1 -a $IOS_PROVISIONING_FILE | pcregrep -o1 '<string>(.*)</string>'`
-
