6ba0f03a539aad00bace3bbc392b6b962de2fb9d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-07 22:32:54 -0300

    Setup logs

--- .buildkite/ios/build.sh
+++ .buildkite/ios/build.sh
@@ -3,6 +3,7 @@ set -e
 cd $ROOT/ios
 
 ARGS=()
+OPTIONS=(CODE_SIGNING_REQUIRED=NO)
 
 if [[ ! -z $IOS_XCCONFIG_FILE ]]; then ARGS+=(-xcconfig $IOS_XCCONFIG_FILE); fi
 
@@ -12,5 +13,6 @@ xcodebuild \
   -workspace $ROOT/ios/EmCasa.xcworkspace \
   -configuration Release \
   -destination "generic/platform=iOS" \
-  ${ARGS[*]} archive | xcpretty
+  ${ARGS[*]} ${OPTIONS[*]} \
+  archive | tee $ROOT/tmp/logs/ios.build.log | xcpretty
 
