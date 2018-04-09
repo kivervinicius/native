94dcbd9fd98bfeedeeb8bfec11a2eae62ea1813a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-09 11:06:22 -0300

    Fix validation flow

--- containers/views/InterestForm/Form.js
+++ containers/views/InterestForm/Form.js
@@ -4,12 +4,6 @@ import Shell, {Footer} from '@/containers/shared/Shell'
 import Form from '@/containers/interest/Form'
 
 export default class InterestFormScreen extends Component {
-  state = {
-    valid: false
-  }
-
-  onValidate = (valid) => this.setState({valid})
-
   onDismiss = () => {
     const {navigation} = this.props
     navigation.goBack(null)
@@ -17,8 +11,10 @@ export default class InterestFormScreen extends Component {
 
   onSubmit = () => {
     const {navigation} = this.props
-    this.form.onSubmit()
-    navigation.navigate('message')
+    if (this.form.onValidate()) {
+      this.form.onSubmit()
+      navigation.navigate('message')
+    }
   }
 
   onOpenCalendly = () => {
@@ -32,19 +28,13 @@ export default class InterestFormScreen extends Component {
 
   render() {
     const {navigation} = this.props
-    const {valid} = this.state
 
     return (
-      <Shell
-        footer={
-          <Footer disabled={!valid} label="Enviar" onPress={this.onSubmit} />
-        }
-      >
+      <Shell footer={<Footer label="Enviar" onPress={this.onSubmit} />}>
         <Form
           innerRef={this.formRef}
           id={navigation.state.params.id}
           onDismiss={this.onDismiss}
-          onValidate={this.onValidate}
           onOpenCalendly={this.onOpenCalendly}
         />
       </Shell>
