55a2b4ead92d9e2620501b1e91cfe1127b1951dd
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-11 17:07:46 -0300

    locked orientation component

---
+++ containers/shared/Orientation/Locked.js
@@ -0,0 +1,51 @@
+import {Component} from 'react'
+import Orientation from 'react-native-orientation'
+
+export default class LockedOrientation extends Component {
+  state = {
+    locked: false
+  }
+
+  static getDerivedStateFromProps(props) {
+    if (props.disabled || !props.to) return {locked: false}
+    return null
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.locked === this.state.locked) return
+    if (this.state.locked) this.onLock(true)
+    else this.onUnlock(true)
+  }
+
+  componentDidMount() {
+    this.onLock(true)
+  }
+
+  componentWillUnmount() {
+    this.onUnlock()
+  }
+
+  onLock = (force = false) => {
+    const {to} = this.props
+    if (!to || (!force && this.state.locked)) return
+    switch (to) {
+      case 'landscape':
+        Orientation.lockToLandscape()
+        break
+      case 'portrait':
+        Orientation.lockToPortrait()
+        break
+    }
+    this.setState({locked: true})
+  }
+
+  onUnlock = (force = false) => {
+    if (!force && !this.state.locked) return
+    Orientation.unlockAllOrientations()
+    this.setState({locked: false})
+  }
+
+  render() {
+    return this.props.children
+  }
+}
--- containers/shared/Shell/index.js
+++ containers/shared/Shell/index.js
@@ -1,12 +1,15 @@
+import Lock from '@/containers/shared/Orientation/Locked'
 import Shell, {Section, Footer} from '@/components/shared/Shell'
 import Header from './Header'
 import Navigation from './Navigation'
 
-export default function ShellApp(props) {
-  return <Shell {...props} />
+export default function ShellApp({lock, ...props}) {
+  const children = <Shell {...props} />
+  return <Lock to={lock}>{children}</Lock>
 }
 
 ShellApp.defaultProps = {
+  lock: 'portrait',
   header: Header
 }
 
