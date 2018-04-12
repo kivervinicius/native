0aab764ed90d64b45f7a3a0d1b28be76da101c3f
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-12 18:42:07 -0300

    Change listings header phrase

--- components/listings/Feed/Results/styles.js
+++ components/listings/Feed/Results/styles.js
@@ -7,16 +7,16 @@ export default StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 15,
     marginBottom: 5
   },
   text: {
+    width: '70%',
     margin: 2,
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: '300',
+    textAlign: 'center',
     color: colors.gray.dark
-  },
-  highlight: {
-    color: colors.blue.medium
   }
 })
