e56182469ed06eeaff2babc840f865ce1a4b764a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-20 14:27:56 -0300

    FormMessage component

---
+++ components/shared/Form/Message/styles.js
@@ -0,0 +1,16 @@
+import StyleSheet from '@/assets/StyleSheet'
+
+export default StyleSheet({
+  container: {
+    padding: 15
+  },
+  text: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: 18
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600'
+  }
+})
