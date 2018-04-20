1db7bf44e980804b9f4b56d925427ac790c9365e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-20 15:13:26 -0300

    Connect navigation auth props

--- containers/shared/Shell/Navigation.js
+++ containers/shared/Shell/Navigation.js
@@ -1,5 +1,16 @@
 import withNavigation from 'react-navigation/src/views/withNavigation'
+import {connect} from 'react-redux'
 
+import {signOut} from '@/redux/modules/auth'
+import {getUser} from '@/redux/modules/auth/selectors'
 import Navigation from '@/components/shared/Shell/Navigation'
 
-export default withNavigation(Navigation)
+const props = (state) => ({
+  user: getUser(state)
+})
+
+const actions = {
+  onLogout: signOut
+}
+
+export default withNavigation(connect(props, actions)(Navigation))
