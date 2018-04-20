92dd60998956e065be0e35d98a93a10053eb384f
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-19 21:09:20 -0300

    remove form state from signup screen

--- containers/views/Auth/SignUp.js
+++ containers/views/Auth/SignUp.js
@@ -1,40 +1,18 @@
-import React, {Component} from 'react'
+import {Component} from 'react'
 
-import Shell, {Footer} from '@/containers/shared/Shell'
+import Shell from '@/containers/shared/Shell'
 import SignUp from '@/containers/auth/SignUp'
 
 export default class SignUpScreen extends Component {
-  state = {
-    valid: true
-  }
-
-  form = React.createRef()
-
-  onValidate = (valid) => this.setState({valid})
-
   onSuccess = () => {
     const {navigation} = this.props
     navigation.goBack(null)
   }
 
-  onSubmit = (...args) => {
-    const {valid} = this.state
-    const form = this.form.value
-    if (valid) form.onSubmit(...args)
-  }
-
   render() {
     return (
-      <Shell
-        scroll
-        title="Cadastre-se"
-        footer={<Footer label="Enviar" onPress={this.onSubmit} />}
-      >
-        <SignUp
-          innerRef={this.form}
-          onValidate={this.onValidate}
-          onSuccess={this.onSuccess}
-        />
+      <Shell scroll title="Cadastre-se">
+        <SignUp onSuccess={this.onSuccess} />
       </Shell>
     )
   }
