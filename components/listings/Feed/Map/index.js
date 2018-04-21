f1885d97491bb5875baa3e84fe7d3895d2e7846b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-21 02:27:27 -0300

    Fix map listings styles

--- components/listings/Feed/Map/index.js
+++ components/listings/Feed/Map/index.js
@@ -3,13 +3,15 @@ import {Dimensions} from 'react-native'
 import Card from '@/components/listings/Card/Map'
 import HorizontalFeed from '../Horizontal'
 
-export default function MapFeed(props) {
+export default function MapFeed({active, ...props}) {
   const width = Dimensions.get('window').width
   return (
     <HorizontalFeed
       {...props}
-      slideWidth={width / 1.5}
-      renderItem={(item) => <Card {...item} />}
+      itemWidth={width / 1.5}
+      renderItem={(item) => (
+        <Card primary={active === item.id} width={width / 1.5} {...item} />
+      )}
     />
   )
 }
