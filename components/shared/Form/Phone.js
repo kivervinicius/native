f9b118cde9a2b750f164e8e867edb31b45f74fea
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-04-22 18:08:42 -0300

    Fix phone validation styles

--- components/shared/Form/Phone.js
+++ components/shared/Form/Phone.js
@@ -2,9 +2,10 @@ import {required, phone} from '@/lib/validations'
 import TextInput from '@/components/shared/TextInput'
 import {field} from './Field'
 
-function Phone({onChange, ...props}) {
+function Phone({onChange, valid, ...props}) {
   return (
     <TextInput
+      invalid={!valid}
       keyboardType="phone-pad"
       placeholder="Telefone"
       onChangeText={onChange}
